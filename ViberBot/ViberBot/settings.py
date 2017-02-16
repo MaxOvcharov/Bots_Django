0c348ffefd0b44b844355df9b4e35bec754c70a3
MaxOvcharov <ovcharovmax@yandex.ru>
2017-02-16 12:05:56 +0300

    Added deploy stuff

--- ViberBot/ViberBot/settings.py
+++ ViberBot/ViberBot/settings.py
@@ -87,13 +87,12 @@ if USE_I18N:
 
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 
 )
