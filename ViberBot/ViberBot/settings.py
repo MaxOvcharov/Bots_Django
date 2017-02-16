ba9c875336bac7a86ac8f9074457cf7a6c21642e
MaxOvcharov <ovcharovmax@yandex.ru>
2017-02-16 14:33:15 +0300

    Added local_settings

--- ViberBot/ViberBot/settings.py
+++ ViberBot/ViberBot/settings.py
@@ -114,8 +114,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_URL = '/static/'
+try:
+    from local_settings import *
+except ImportError:
+    pass
