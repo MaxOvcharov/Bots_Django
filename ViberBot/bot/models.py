5304e6dd0533e959db28647612160d06c5e0dd22
MaxOvcharov <ovcharovmax@yandex.ru>
2017-02-17 12:37:19 +0300

    Added models for parser

--- ViberBot/bot/models.py
+++ ViberBot/bot/models.py
@@ -1,5 +1,28 @@
-from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+
+class Cities(models.Model):
+    city_name = models.CharField(max_length=40, verbose_name="Название города")
+    city_url = models.TextField(verbose_name="Ссылка на город")
+    author = models.CharField(max_length=60, verbose_name="Автор фотографий")
+
+    def __str__(self):
+        return "%s %s" % (self.city_name, self.author)
+
+    class Meta:
+        ordering = ["city_name"]
+        verbose_name = "Города"
+
+
+class CityPhotos(models.Model):
+    photo_url = models.TextField(verbose_name="Ссылка на фото")
+    city = models.ForeignKey(Cities, on_delete=models.CASCADE, verbose_name="ID города")
+
+    def __str__(self):
+        return self.photo_url
+
+    class Meta:
+        verbose_name = "Города"
