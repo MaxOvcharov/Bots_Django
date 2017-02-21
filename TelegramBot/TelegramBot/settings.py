1adf3637554f4772fc854435159729955ef89548
MaxOvcharov <ovcharovmax@yandex.ru>
2017-02-21 15:32:55 +0000

    Stable version

--- TelegramBot/TelegramBot/settings.py
+++ TelegramBot/TelegramBot/settings.py
@@ -19,15 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$tn_xprstsp(@0u+k)j@w%whba+7b%7^g-5)gvo618wc1iuixu'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -68,17 +59,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'TelegramBot.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
