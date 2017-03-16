46905ad2b07daab8e65429e9d2b5ba4f74834cb0
MaxOvcharov <ovcharovmax@yandex.ru>
2017-03-16 14:58:03 +0300

    Added city_polls table to admin site

--- TelegramBot/bot/admin.py
+++ TelegramBot/bot/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from models import Cities, CityPhotos, DialogStepRouting, UserInfo
+from models import Cities, CityPhotos, DialogStepRouting, UserInfo, CityPolls
 # Register your models here.
 
 admin.site.register(Cities)
 admin.site.register(CityPhotos)
 admin.site.register(DialogStepRouting)
 admin.site.register(UserInfo)
+admin.site.register(CityPolls)
