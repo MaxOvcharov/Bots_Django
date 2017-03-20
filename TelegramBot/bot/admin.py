b6aa6a1114b07072d6e4931422ec4b5e9c846666
MaxOvcharov <ovcharovmax@yandex.ru>
2017-03-20 16:23:11 +0300

    Added NewsPoll table to the django admin site

--- TelegramBot/bot/admin.py
+++ TelegramBot/bot/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
 from models import Cities, CityPhotos, DialogStepRouting,\
-                   UserInfo, CityPoll, News
+                   UserInfo, CityPoll, News, NewsPoll
 # Register your models here.
 
 admin.site.register(Cities)
 admin.site.register(CityPhotos)
 admin.site.register(DialogStepRouting)
 admin.site.register(News)
+admin.site.register(NewsPoll)
 admin.site.register(UserInfo)
 admin.site.register(CityPoll)
